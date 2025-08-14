87c13d23cd96166cf4ecbcab726b2c0bbd628169
Cloud Hu <cloudhu9527@gmail.com>
2025-08-14 08:34:18 +0800

    Setup Player

---
+++ Source/EpicSquad/Private/GlobalVars/GlobalTags.cpp
@@ -0,0 +1,15 @@
+﻿// // CloudHu:604746493@qq.com All Rights Reserved
+
+
+#include "GlobalVars/GlobalTags.h"
+
+
+namespace GlobalGameplayTags
+{
+	//Factions
+	UE_DEFINE_GAMEPLAY_TAG(Factions_ID_Alliance,"Factions.ID.Alliance");
+	UE_DEFINE_GAMEPLAY_TAG(Factions_ID_Horde,"Factions.ID.Horde");
+	
+
+	
+}
---
+++ Source/EpicSquad/Public/GlobalVars/GlobalTags.h
@@ -0,0 +1,14 @@
+﻿// // CloudHu:604746493@qq.com All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GlobalGameplayTags
+{
+	//Factions
+	EPICSQUAD_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Factions_ID_Alliance);
+	EPICSQUAD_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Factions_ID_Horde);
+
+
+}
